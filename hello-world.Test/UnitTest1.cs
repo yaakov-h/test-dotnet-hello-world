6c59edc9f8ed20acf90a567864a238b4574d99df
Yaakov <git@yaakov.online>
2018-12-30 14:55:32 +1100

    Create project with test

---
+++ hello-world.Test/UnitTest1.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+using System.Text;
+using NUnit.Framework;
+using hello_world;
+
+namespace hello_world.Test
+{
+    public class ProgramTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Program.Main(Array.Empty<string>());
+            Assert.That(standardOutText.ToString(), Is.EqualTo("Hello World!" + Environment.NewLine));
+
+        }
+
+        StringBuilder standardOutText;
+        TextWriter originalStandardOut;
+
+        [SetUp]
+        public void Setup()
+        {
+            originalStandardOut = Console.Out;
+
+            standardOutText = new StringBuilder();
+            var writer = new StringWriter(standardOutText);
+            Console.SetOut(writer);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Console.SetOut(originalStandardOut);
+            originalStandardOut = null;
+            standardOutText = null;
+        }
+    }
+}
